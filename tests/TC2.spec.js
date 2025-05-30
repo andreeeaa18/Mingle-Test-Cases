f34fb34158946f874ff1d83cf0beb06825959966
andreeeaa18 <andreevandreea.18@gmail.com>
2025-05-30 16:48:55 +0300

    TC2 modified

--- tests/TC2.spec.js
+++ tests/TC2.spec.js
@@ -3,14 +3,14 @@ import { test, expect } from "@playwright/test";
 test("Sign up", async ({ page }) => {
   await page.goto("https://frontend-mingle.vercel.app");
 
-  await page.locator('a:has-text("LOGIN")').click();
+  await page.locator('a:has-text("Login")').click();
   await page.locator('a:has-text("Sign up")').click();
 
   await page.locator('[name="name"]').fill("Andreea");
   await page.locator('[name="email"]').fill("andreevandreea.18@gmail.com");
   await page.locator('[name="password"]').fill("1234554321");
   await page.locator('[name="confirm_password"]').fill("1234554321");
-  await page.locator('button:has-text("SIGN UP")').click();
+  await page.locator('button:has-text("Sing up")').click();
 
   // await page
   //   .locator("text=Signed in successfully! ")
